d6b07cb40379f427eda1ab99f645f29c92615129
Danil Rezyayev <danil.rezyaev@gmail.com>
2021-01-24 17:47:36 +0300

    move header to custom document file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
 					<link rel="stylesheet" href="/fonts/fonts.css" />
 				</Head>
 				<Body>
+					<Header />
 					<Main />
 					<NextScript />
 				</Body>
@@ -24,5 +25,28 @@ function Body({ children }: BodyProps) {
 			<div className="absolute right-0 z-0 w-1/3 h-full bg-body-border-light"></div>
 			<div className="relative bg-white h-body w-body">{children}</div>
 		</body>
+	);
+}
+
+function Header() {
+	return (
+		<header className="flex justify-between pt-9 px-9">
+			<div className="flex">
+				<div className="mt-1 mr-8 bg-black rounded-full w-14 h-14"></div>
+				<div className="text-xl">
+					<h1 className="font-bold">Yevgeniy Astrakhantsev</h1>
+					<h2 className="font-medium">
+						Independent Animation Director <br /> & Animator
+					</h2>
+				</div>
+			</div>
+
+			<nav className="text-xl font-medium">
+				<a className="mr-8" href="">
+					Work
+				</a>
+				<a href="">About</a>
+			</nav>
+		</header>
 	);
 }
